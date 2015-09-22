69a33f9654beeb685ca43a11a2f6fec9bbfd04a0
danielZhang0601 <daniel.zhang@wolaidai.com>
2015-09-22 17:00:31 +0800

    first

---
+++ Family_iOS/Family_iOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Family_iOS
+//
+//  Created by Daniel.Zhang on 15/9/22.
+//  Copyright © 2015年 Daniel.Zhang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
