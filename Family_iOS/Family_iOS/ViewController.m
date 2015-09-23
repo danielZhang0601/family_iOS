f47860d229647e5e71fb0cf2029ed94869ab88db
danielZhang0601 <daniel.zhang@wolaidai.com>
2015-09-23 22:57:38 +0800

    2015-09-23

--- Family_iOS/Family_iOS/ViewController.m
+++ Family_iOS/Family_iOS/ViewController.m
@@ -2,8 +2,8 @@
 //  ViewController.m
 //  Family_iOS
 //
-//  Created by Daniel.Zhang on 15/9/22.
-//  Copyright © 2015年 Daniel.Zhang. All rights reserved.
+//  Created by zhengyixiong on 15/9/22.
+//  Copyright © 2015年 zxd. All rights reserved.
 //
 
 #import "ViewController.h"
