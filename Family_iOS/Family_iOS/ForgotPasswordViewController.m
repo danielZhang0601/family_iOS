337b4d16851e0f94f9a5876e1dd40a156abf79eb
danielZhang0601 <daniel.zhang@wolaidai.com>
2015-10-08 22:57:54 +0800

    2015-10-08

--- Family_iOS/Family_iOS/ForgotPasswordViewController.m
+++ Family_iOS/Family_iOS/ForgotPasswordViewController.m
@@ -7,8 +7,20 @@
 //
 
 #import "ForgotPasswordViewController.h"
+#import "NSString+Utils.h"
+#import "MBProgressHUD.h"
+#import "CMServerAccountProtocol.h"
 
 @interface ForgotPasswordViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *accountTextField;
+@property (weak, nonatomic) IBOutlet UITextField *verifitionCodeTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+- (IBAction)getVerifitionCode:(UIButton *)sender;
+- (IBAction)resetPassword:(UIButton *)sender;
+- (IBAction)accountEditChanged:(UITextField *)sender;
+- (IBAction)verifitionCodeEditChanged:(UITextField *)sender;
+- (IBAction)passwordEditChanged:(UITextField *)sender;
 
 @end
 
@@ -34,4 +46,89 @@
 }
 */
 
+- (IBAction)getVerifitionCode:(UIButton *)sender {
+    NSString *account = self.accountTextField.text;
+    
+    if ([account isEmptyOrNull]) {
+        NSLog(@"account null");
+        return;
+    }else if (![account checkIsMachesRegex:CELL_PHONE_REG]) {
+        NSLog(@"account not match");
+        return;
+    }
+    
+    MBProgressHUD* HUD = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    HUD.labelText = @"Get Code";
+    
+    [CMServerAccountProtocol request_reset_password_by_phoneByAccount:account WithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [MBProgressHUD hideAllHUDsForView:self.view animated:NO];
+        if ([[responseObject objectForKey:@"code"] intValue] == 0) {
+            NSLog(@"send sms success");
+        } else {
+            NSLog(@"Ret Error:%@", responseObject);
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        [MBProgressHUD hideAllHUDsForView:self.view animated:NO];
+        NSLog(@"Error: %@", error);
+    }];
+}
+
+- (IBAction)resetPassword:(UIButton *)sender {
+    NSString *account = self.accountTextField.text;
+    NSString *verifitionCode = self.verifitionCodeTextField.text;
+    NSString *password = self.passwordTextField.text;
+    
+    if ([account isEmptyOrNull]) {
+        NSLog(@"account null");
+        return;
+    } else if (![account checkIsMachesRegex:CELL_PHONE_REG]) {
+        NSLog(@"account not match");
+        return;
+    } else if ([verifitionCode isEmptyOrNull]) {
+        NSLog(@"verifitionCode null");
+        return;
+    } else if (![verifitionCode checkIsMachesRegex:VERIFY_CODE_REG]) {
+        NSLog(@"verifitionCode not match");
+        return;
+    } else if ([password isEmptyOrNull]) {
+        NSLog(@"password null");
+        return;
+    } else if (![password checkIsMachesRegex:PASSWORD_REG]) {
+        NSLog(@"password not match");
+        return;
+    }
+    
+    MBProgressHUD* HUD = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    HUD.labelText = @"Create User";
+    
+    [CMServerAccountProtocol reset_passwordByAccount:account Password:password ResetCode:verifitionCode WithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [MBProgressHUD hideAllHUDsForView:self.view animated:NO];
+        if ([[responseObject objectForKey:@"code"] intValue] == 0) {
+            NSLog(@"regist success");
+        } else {
+            NSLog(@"Ret Error:%@", responseObject);
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        [MBProgressHUD hideAllHUDsForView:self.view animated:NO];
+        NSLog(@"Error: %@", error);
+    }];
+}
+
+- (IBAction)accountEditChanged:(UITextField *)sender {
+    if (sender.text.length > CELL_PHONE_LENGTH) {
+        sender.text = [sender.text substringToIndex:CELL_PHONE_LENGTH];
+    }
+}
+
+- (IBAction)verifitionCodeEditChanged:(UITextField *)sender {
+    if (sender.text.length > VERIFY_CODE_LENGTH) {
+        sender.text = [sender.text substringToIndex:VERIFY_CODE_LENGTH];
+    }
+}
+
+- (IBAction)passwordEditChanged:(UITextField *)sender {
+    if (sender.text.length > PASSWORD_MAX_LENGTH) {
+        sender.text = [sender.text substringToIndex:PASSWORD_MAX_LENGTH];
+    }
+}
 @end
--- Family_iOS/Family_iOS/SignUpViewController.m
+++ Family_iOS/Family_iOS/SignUpViewController.m
@@ -8,11 +8,15 @@
 
 #import "SignUpViewController.h"
 #import "NSString+Utils.h"
+#import "MBProgressHUD.h"
+#import "CMServerAccountProtocol.h"
 
 @interface SignUpViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *accountTextField;
 @property (weak, nonatomic) IBOutlet UITextField *verifitionCodeTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIButton *getCodeBtn;
+@property (weak, nonatomic) IBOutlet UIButton *doneBtn;
 
 
 - (IBAction)getVerifitionCode:(id)sender;
@@ -47,10 +51,71 @@
 */
 
 - (IBAction)getVerifitionCode:(id)sender {
+    NSString *account = self.accountTextField.text;
     
+    if ([account isEmptyOrNull]) {
+        NSLog(@"account null");
+        return;
+    }else if (![account checkIsMachesRegex:CELL_PHONE_REG]) {
+        NSLog(@"account not match");
+        return;
+    }
+    
+    MBProgressHUD* HUD = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    HUD.labelText = @"Get Code";
+    
+    [CMServerAccountProtocol request_register_smsByCellPhone:account WithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [MBProgressHUD hideAllHUDsForView:self.view animated:NO];
+        if ([[responseObject objectForKey:@"code"] intValue] == 0) {
+            NSLog(@"send sms success");
+        } else {
+            NSLog(@"Ret Error:%@", responseObject);
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        [MBProgressHUD hideAllHUDsForView:self.view animated:NO];
+        NSLog(@"Error: %@", error);
+    }];
 }
 
 - (IBAction)createNewAccount:(id)sender {
+    NSString *account = self.accountTextField.text;
+    NSString *verifitionCode = self.verifitionCodeTextField.text;
+    NSString *password = self.passwordTextField.text;
+    
+    if ([account isEmptyOrNull]) {
+        NSLog(@"account null");
+        return;
+    } else if (![account checkIsMachesRegex:CELL_PHONE_REG]) {
+        NSLog(@"account not match");
+        return;
+    } else if ([verifitionCode isEmptyOrNull]) {
+        NSLog(@"verifitionCode null");
+        return;
+    } else if (![verifitionCode checkIsMachesRegex:VERIFY_CODE_REG]) {
+        NSLog(@"verifitionCode not match");
+        return;
+    } else if ([password isEmptyOrNull]) {
+        NSLog(@"password null");
+        return;
+    } else if (![password checkIsMachesRegex:PASSWORD_REG]) {
+        NSLog(@"password not match");
+        return;
+    }
+    
+    MBProgressHUD* HUD = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    HUD.labelText = @"Create User";
+    
+    [CMServerAccountProtocol sms_registerByAccount:account Password:password Sms_code:verifitionCode WithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [MBProgressHUD hideAllHUDsForView:self.view animated:NO];
+        if ([[responseObject objectForKey:@"code"] intValue] == 0) {
+            NSLog(@"regist success");
+        } else {
+            NSLog(@"Ret Error:%@", responseObject);
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        [MBProgressHUD hideAllHUDsForView:self.view animated:NO];
+        NSLog(@"Error: %@", error);
+    }];
 }
 
 - (IBAction)accountEditChanged:(UITextField *)sender {
@@ -68,8 +133,6 @@
 - (IBAction)passwordEditChanged:(UITextField *)sender {
     if (sender.text.length > PASSWORD_MAX_LENGTH) {
         sender.text = [sender.text substringToIndex:PASSWORD_MAX_LENGTH];
-    }else if (sender.text.length < PASSWORD_MIN_LENGTH) {
-        
     }
 }
 
